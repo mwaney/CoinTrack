b5fc04fc8cda5842822686104200ef8b3a13b878
mwaney <emwaniki91@gmail.com>
2023-04-08 22:26:43 +0300

    Add more functionality and finish the coding key features

--- CoinTrack/src/components/AddTransaction.jsx
+++ CoinTrack/src/components/AddTransaction.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+    const { addTransaction } = useContext(GlobalContext);
 
     const handleTextChange = (e) => setText(e.currentTarget.value);
     const handleAmountChange = (e) => setAmount(e.currentTarget.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        
+        const newTransaction = {
+            id: Math.floor(Math.random() * 10000000),
+            text: text,
+            amount: +amount,
+        }
+        addTransaction(newTransaction);
+    }
   return (
     <>
     <h3>Add New Transaction</h3>
-    <form>
+    <form onSubmit={onSubmit}>
         <div className="form-control">
             <label htmlFor="text">Text</label>
             <input type="text" value={text} onChange={handleTextChange} placeholder='Enter text...'/>
--- CoinTrack/src/components/IncomeExpenses.jsx
+++ CoinTrack/src/components/IncomeExpenses.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  const positive = transactions.filter(transaction => transaction.amount > 0);
+  const negative = transactions.filter(transaction => transaction.amount < 0);
+  const posTotal = positive.reduce((acc, item) => (acc + item.amount),0).toFixed(2);
+  let negTotal = negative.reduce((acc, item) => (acc + item.amount),0).toFixed(2);
+  negTotal *= -1;
   return (
     <div className="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p className='money plus'>+$0.00</p>
+            <p className='money plus'>+${posTotal}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p className='money minus'>-$0.00</p>
+            <p className='money minus'>-${negTotal}</p>
         </div>
     </div>
     
